b042adcdf7cfddc27015716833b9e80a7c6e4757
jpjp2328 <110818668+jpjp2328@users.noreply.github.com>
2022-11-07 00:24:22 +1100

    index.js updated, inquirer prompt added

--- index.js
+++ index.js
@@ -1,5 +1,88 @@
-// Dependencies
+// Import dependencies
 const inquirer = require('inquirer');
 const mysql = require('mysql2')
 const consoleTable = require('console.table')
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Jpjp2328!@#',
+        database: 'employee_db'
+    }
+);
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('connected to employee_db database.');
+    init();
+});
+
+// Menu/Main Page/Inquirer
+function init() {
+    inquirer.prompt ({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Departments',
+            'View All Roles',
+            'View All Employees',
+            'Add Department',
+            'Add Role',
+            'Add Employee',
+            'Update Employee Role',
+            'Quit',
+        ],
+    }).then((answer) => {
+        switch (answer.action) {
+            case 'View All Departments':
+                viewDepartment();
+                break;
+
+            case 'View All Roles':
+                viewRole();
+                break;
+            
+            case 'View All Employees':
+                viewEmployee();
+                break;
+
+            case 'Add Department':
+                addDepartment();
+                break;
+
+            case 'Add Role':
+                addRole();
+                break;
+
+            case 'Add Employee':
+                addEmployee();
+                break;
+
+            case 'Update Employee Role':
+                updateEmployee();
+                break;
+
+            case 'Quit':
+                db.end();
+                break;
+        }
+    });
+};
+
+// View Department Function
+
+// View Roles Function
+
+// View Employees Function
+
+// Add Department Function
+
+// Add Role Function
+
+// Add Employee Function
+
+// Update Employee Role Function
 
